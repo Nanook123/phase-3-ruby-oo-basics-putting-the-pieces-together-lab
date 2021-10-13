d336183af05180cb2c2adf37eb5c66a564720eef
Joshua Allford <joshallfordnanook@gmail.com>
2021-10-14 00:31:39 +0100

    made it way harder than it needed to be

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title = "And Then There Were None")
+        @title = title
+    end
+    def author(author = "Agatha Christie")
+        @author = author
+    end
+    def page_count(page_count = 272)
+        @page_count = page_count
+    end
+    def genre(genre = "Mystery")
+        @genre = genre
+    end
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+end
